0085c2276da72ea8399926974545ebb8fac507eb
Mahammad Makhmaev <mmakhmae@c2r1p2.42nice.fr>
2026-06-09 17:09:12 +0200

    codexion debut

--- main.c
+++ main.c
@@ -1,3 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmakhmae <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/09 15:46:57 by mmakhmae          #+#    #+#             */
+/*   Updated: 2026/06/09 15:47:08 by mmakhmae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <pthread.h>
+
+
+int create_thread(int c)
+{
+    pthread_create()
+    return ;
+}
+
+
+
 int main(int argc, char **argv)
 {
     if (argc > 8)

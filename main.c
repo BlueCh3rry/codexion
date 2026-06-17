04ad5eaf8b96babb12178e7618b85ab042c9d0c9
Mahammad Makhmaev <mmakhmae@c3r2p2.42nice.fr>
2026-06-17 21:10:24 +0200

    upd thread

--- main.c
+++ main.c
@@ -26,7 +26,8 @@ typedef struct coder
 {
     int id;
     long last_compile_start;
-    int compile_count;
+
+	t_data      *data;
 
     t_d *left;
     t_d *right;
@@ -36,7 +37,15 @@ typedef struct coder
 
 typedef struct data
 {
-    int stop;
+	long start_time;
+	unsigned int number_of_coders;
+	unsigned int time_to_burnout;
+	unsigned int time_to_compile;
+	unsigned int time_to_debug;
+	unsigned int time_to_refactor;
+	unsigned int number_of_compiles_required;
+	unsigned int dongle_cooldown;
+	char			*scheduler;
 
     t_c *coders;
     t_d *dongles;
@@ -60,11 +69,9 @@ size_t	ft_strlcpy(char *dst, const char *src)
 	return (j);
 }
 
-int	check_sched(char *s)
+int check_sched(char *s)
 {
-	if (strcmp(s, "fifo") || strcmp(s, "edf"))
-		return (0);
-	return (1);
+    return (!strcmp(s, "fifo") || !strcmp(s, "edf"));
 }
 
 long	current_time_ms(void)
@@ -75,49 +82,48 @@ long	current_time_ms(void)
     return (tv.tv_sec * 1000L) + (tv.tv_usec / 1000L);
 }
 
-void	show_log(long s, int nb, char *m)
+void	log_state(t_data *data, int id, char *msg)
 {
-	printf("%ld %d %s\n", current_time_ms() - s, nb, m);
+	pthread_mutex_lock(&data->log_mutex);
+	printf("%ld %d %s\n",
+		current_time_ms() - data->start_time,
+		id,
+		msg);
+	pthread_mutex_unlock(&data->log_mutex);
 }
 
-void	*test_func(void *arg)
+void	*coder_routine(void *arg)
 {
-	pthread_mutex_lock(&g_mutex);
-	printf("Hello from the new thread!\n");
-	pthread_mutex_unlock(&g_mutex);
+	t_c *coder;
+
+    coder = (t_c *)arg;
+	if (coder->left && coder->right)
+	{
+		pthread_mutex_lock(&coder->left->mutex);
+		log_state(&coder->data->start_time, coder->id, "has taken a dongle");
+		usleep(&coder->data->dongle_cooldown);
+		pthread_mutex_lock(&coder->right->mutex);
+		log_state(&coder->data->start_time, coder->id, "has taken a dongle");
+	}
+	log_state(&coder->data->start_time, coder->id, "is compiling");
+	coder->last_compile_start = current_time_ms();
+	usleep(&coder->data->time_to_compile);
+	log_state(&coder->data->start_time, coder->id, "is debugging");
+	usleep(&coder->data->time_to_debug);
+	log_state(&coder->data->start_time, coder->id, "is refactoring");
+	usleep(&coder->data->time_to_refactor);
+
+	if (!strcmp(coder->data->scheduler, "edf"))
+	{
+		long deadline = coder->last_compile_start + coder->data->time_to_burnout;
+		if (current_time_ms() - coder->last_compile_start == deadline)
+		{
+			usleep(&coder->data->time_to_burnout);
+			log_state(&coder->data->start_time, coder->id, "burned out");
+			exit(0);
+		}
+	}
 	return (NULL);
-}
-
-int coder_routine()
-{
-	int count_d;
-
-	count_d = 0;
-	while (count_d != 2 && dongles != 0)
-	{
-		show_log(start, coder, "has taken a dongle");
-		dongles--;
-		count_d++;
-		if (count_d < 2)
-			usleep(dongle_cooldown);
-	}
-	if (count_d > 0)
-	{
-		show_log(start, coder, "is compiling");
-		usleep(time_to_compile);
-		dongles += count_d;
-		show_log(start, coder, "is debugging");
-		usleep(time_to_debug);
-		show_log(start, coder, "is refactoring");
-		usleep(time_to_refactor);
-	}
-	if (coder == number_of_compiles_required || count_d == 0)
-	{
-		usleep(time_to_burnout);
-		show_log(start, coder, "burned out");
-		return(1);
-	}
-	return (0);
 }
 
 int	main(int argc, char **argv)
@@ -128,19 +134,12 @@ int	main(int argc, char **argv)
 		return (0);
 	}
 	printf("\n===Codexion===\n\n");
-	unsigned int	number_of_coders;
-	unsigned int	time_to_burnout;
-	unsigned int	time_to_compile;
-	unsigned int	time_to_debug;
-	unsigned int	time_to_refactor;
-	unsigned int	number_of_compiles_required;
-	unsigned int	dongle_cooldown;
-	char			*scheduler;
-	pthread_t		thid;
+
+	t_c				*coders;
+	t_d				*dongles;
+	t_data			data;
 	void			*ret;
-	int				dongles;
-	int				coder;
-	int				count_d;
+	int				i;
 	long			start;
 
 	if (atoi(argv[1]) <= 0)
@@ -148,81 +147,82 @@ int	main(int argc, char **argv)
 		printf("Error [1] number of coders is under 1");
 		return (0);
 	}
-	number_of_coders = atoi(argv[1]);
+	data.number_of_coders = atoi(argv[1]);
 	if (atoi(argv[2]) <= 0)
 	{
 		printf("Error [2] time to burnout is under 1");
 		return (0);
 	}
-	time_to_burnout = atoi(argv[2]) * 1000;
+	data.time_to_burnout = atoi(argv[2]) * 1000;
 	if (atoi(argv[3]) <= 0)
 	{
 		printf("Error [3] time to compile is under 1");
 		return (0);
 	}
-	time_to_compile = atoi(argv[3]) * 1000;
+	data.time_to_compile = atoi(argv[3]) * 1000;
 	if (atoi(argv[4]) <= 0)
 	{
 		printf("Error [4] time to debug is under 1");
 		return (0);
 	}
-	time_to_debug = atoi(argv[4]) * 1000;
+	data.time_to_debug = atoi(argv[4]) * 1000;
 	if (atoi(argv[5]) <= 0)
 	{
 		printf("Error [5] time to refactor is under 1");
 		return (0);
 	}
-	time_to_refactor = atoi(argv[5]) * 1000;
+	data.time_to_refactor = atoi(argv[5]) * 1000;
 	if (atoi(argv[6]) <= 0)
 	{
 		printf("Error [6] number of compiles required is UNDER 1");
 		return (0);
 	}
-	else if (atoi(argv[6]) > number_of_coders)
+	else if (atoi(argv[6]) > data.number_of_coders)
 	{
 		printf("Error [6] number of compiles required is ABOVE the number of coders");
 		return (0);
 	}
-	number_of_compiles_required = atoi(argv[6]);
+	data.number_of_compiles_required = atoi(argv[6]);
 	if (atoi(argv[7]) < 0)
 	{
 		printf("Error [7] dongle cooldown is under 0");
 		return (0);
 	}
-	dongle_cooldown = atoi(argv[7]) * 1000;
-	if (check_sched(argv[8]))
+	data.dongle_cooldown = atoi(argv[7]) * 1000;
+	if (!check_sched(argv[8]))
 	{
-		printf("Error [8] scheduler is neither fifo/edf");
+		printf("Error [8] scheduler is neither fifo nor edf\n");
 		return (0);
 	}
-	scheduler = malloc(strlen(argv[8]));
-	if (!scheduler)
+	data.scheduler = malloc(strlen(argv[8]) + 1);
+	if (!data.scheduler)
 	{
 		printf("Error malloc scheduler");
 		return (0);
 	}
-	ft_strlcpy(scheduler, argv[8]);
+	ft_strlcpy(data.scheduler, argv[8]);
 
 	// Program starts here ##############################
 
-	thid = malloc(sizeof(pthread_t) * number_of_coders);
-	int i = 0;
-	coder = 1;
-	dongles = number_of_coders;
-	start = current_time_ms();
-	while (i <= number_of_coders)
+	coders = malloc(sizeof(t_c) * data.number_of_coders);
+	dongles = malloc(sizeof(t_d) * data.number_of_coders);
+	i = 0;
+	data.start_time = current_time_ms();
+	while (i <= data.number_of_coders)
 	{
-    	pthread_create(&thid[i], NULL, coder_routine, &coders[i]);
+		coders[i].id = i;
+		coders[i].data = &data;
+    	pthread_create(&coders[i].thread, NULL, coder_routine, &coders[i]);
 		i++;
 	}
 	while (i >= 0)
 	{
-		pthread_join(thid, &ret);
+		pthread_join(&coders[i].thread, &ret);
 		i--;
 	}
 	printf("thread exited with '%p'\n", ret);
-	free(scheduler);
-	free(thid);
+	free(data.scheduler);
 	free(coders);
+	free(dongles);
 	return (0);
 }
